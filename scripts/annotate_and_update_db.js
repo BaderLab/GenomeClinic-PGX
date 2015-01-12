cd286f2a4f532b0db051b80996948f41e1cfeb1d
Patrick Magee <patrickmageee@gmail.com>
2015-01-12 16:57:12 -0500

    Partially completed asynchronous script for annotating and adding input vcf files to a database

--- scripts/annotate_and_update_db.js
+++ scripts/annotate_and_update_db.js
@@ -1,34 +1,13 @@
 var Promise = require("../node_modules/bluebird");
 var db = require("../frangipani_node_modules/DB");
-//var mongo = require("../node_modules/mongodb"); // this script must be run from the main fragnipani page
 var fs = Promise.promisifyAll(require('fs'));
-var Getopt = require("../node_modules/node-getopt")
 var path = require("path");
-var glob = Promise.promisifyAll(require("../node_modules/glob"))
+var glob = Promise.promisifyAll(require("../node_modules/glob"));
 var child_process=Promise.promisifyAll(require('child_process'));
-function InputError(message){
-	this.name = "InputError";
-	this.message = ( message || "" );
-}
-InputError.prototype = Object.create(Error.prototype);
+var lineReader = Promise.promisifyAll(require('../node_modules/line-reader'));
 
-function InvalidArgument(message){
-	this.name = "InvalidArgument";
-	this.message = ( message || "" );
-}
-InvalidArgument.prototype = Object.create(Error.prototype);
 
-function AnnotationError(message){
-	this.name = "AnnotationError";
-	this.message = ( message || "" );
-}
-AnnotationError.prototype = Object.create(Error.prototype);
 
-function ParseError(message){
-	this.name = "ParseError";
-	this.message = ( message || "" );
-}
-ParseError.prototype = Object.create(Error.prototype);
 
 
 /* This script will annotate and add a specified vcf file to a speceific mongodb. This is a temporary
@@ -40,73 +19,93 @@ ParseError.prototype = Object.create(Error.prototype);
 
 
 
-var main = function(){ 
-	promise = new Promise(function(resolve,reject){
-		var annovarCmd;
-		var parserCmd;
-		var cmdOpts = new Getopt([
-			['a','annovarpath=ARG',"Path to Annovar Directory. Required"],
-			['i','input=ARG','Input Vcf file. Required'],
-			['o','output=ARG','Output file name. Required'],
-			['t','table=ARG','New Table Name. Required (for now)'],
-			['d','database=ARG','specify databses (protocol) to use. if no option given uses default'],
-			['u','dbusage=ARG','specify operations to use if -db option given'],
-			['p','perl=ARG','path to perl exectuable'],
-			['P','python=ARG','path to python executable'],
-			['h','help','display this help']])
-		.bindHelp();
-		var opts = cmdOpts.parseSystem();
-		//console.info(opts);
 
-		//check for  required arguments and set variables to argument
-		if (!opts.options['annovarpath']){
-			cmdOpts.showHelp();
-			reject(new InvalidArgument("AnnovarPath not provided"));
+
+
+
+
+
+var annotateAndAddVariants = function(options){
+	var InputError = function(message){
+		this.name = "InputError";
+		this.message = ( message || "" );
+	}
+	InputError.prototype = Object.create(Error.prototype);
+
+	var InvalidArgument = function(message){
+		this.name = "InvalidArgument";
+		this.message = ( message || "" );
+	}
+	InvalidArgument.prototype = Object.create(Error.prototype);
+
+	var AnnotationError = function(message){
+		this.name = "AnnotationError";
+		this.message = ( message || "" );
+	}
+	AnnotationError.prototype = Object.create(Error.prototype);
+
+	var ParseError = function(message){
+		this.name = "ParseError";
+		this.message = ( message || "" );
+	}
+	ParseError.prototype = Object.create(Error.prototype);
+
+	var MongoError = function(message){
+		this.name = "MongoError";
+		this.message = ( message || "" );
+	}
+	MongoError.prototype = Object.create(Error.prototype);
+
+
+	var promise = new Promise(function(resolve,reject){
+		/*
+		if (!options['annovarpath']){
+			annovarPath = '/Users/patrickmagee/Tools/annovar'
+			//throw new InvalidArgument("AnnovarPath not provided");
 		} else {
-			var annovarPath = path.resolve(opts.options['annovarpath']);
+			var annovarPath = path.resolve(options['annovarpath']);
 		}
-		if (!opts.options['input']){
-			cmdOpts.showHelp();
-			reject(new InvalidArgument("Input file path not provided"));
+		
+		if (!options['input']){
+			throw new InvalidArgument("Input file path not provided");
 		} else {
-			var inputFile = path.resolve(opts.options['input']);
+			var inputFile = path.resolve(options['input']);
 			var tempOutputFile = inputFile + '.hg19_multianno.txt';
 		}
-		if (!opts.options['table']){
-			cmdOpts.showHelp();
-			reject(new InvalidArgument("Table name not provided"));
+		if (!options['table']){
+			throw new InvalidArgument("Table name not provided");
 		} else {
-			var tableName = opts.options['table'];
+			var tableName = options['table'];
 		}
-		if (!opts.options['output']){
-			cmdOpts.showHelp();
-			reject(new InvalidArgument("Output file path not provided"));
+
+		if (!options['output']){
+			throw new InvalidArgument("Output file path not provided");
 		} else {
-			var outputFile = path.resolve(opts.options['output']);
-		} if (opts.options['database'] & !opts.options['dbusage'] ||
-			!opts.options['database'] & opts.options['dbusage']){
-			cmdOpts.showHelp();
-			reject(new InvalidArgument("Database options provide, please provide dbusage AND database"));
+			var outputFile = path.resolve(options['output']);
+		} if (options['database'] & !options['dbusage'] ||
+			!options['database'] & options['dbusage']){
+			throw new InvalidArgument("Database options provide, please provide dbusage AND database");
 		}
+
 		var databaseString;
 		var dbusageString;
 
-		if (opts.options['database']){
-			var count1 = (opts.options['database'].match(/,/g) || []).length;
-			var count2 = (opts.options['dbusage'].match(/,/g) || []).length;
+		if (options['database']){
+			var count1 = (options['database'].match(/,/g) || []).length;
+			var count2 = (options['dbusage'].match(/,/g) || []).length;
 			if (count1 !== count2){
-				cmdOpts.showHelp();
-				reject(new InvalidArgument("length of -database and -dbusage options does not match!"));
+				throw new InvalidArgument("length of -database and -dbusage options does not match!");
 			} else {
-				databaseString = opts.options['database'];
-				dbusageString = opts.options['dbusage'];
+				databaseString = options['database'];
+				dbusageString = options['dbusage'];
 			}
 
 		} else {
 			databaseString = 'refgene,esp6500_all,ljb26_all,clinvar_20140929,cg69,1000g2014sep_amr,1000g2014sep_eas,1000g2014sep_afr,1000g2014sep_eur,1000g2014sep_sas,1000g2014sep_all';
 			dbusageString = 'g,f,f,f,f,f,f,f,f,f,f';
 		}
-
+		*/
+		/*
 		var PYTHONPATH;
 		if(opts.options['python']){
 			PYTHONPATH = path.resolve(opts.options['python']);	
@@ -119,55 +118,116 @@ var main = function(){
 		} else {
 			PERLPATH = 'perl'
 		}
+		*/
 
 		//check to see if file exists
 
-		fs.statAsync(inputFile)
-		.then(fs.statAsync(annovarPath))
-		.then(function(){
-			
 
-			var annovarCmd = PERLPATH + ' '  + annovarPath + '/table_annovar.pl ' +  
+		var annovarPath = '/Users/patrickmagee/Tools/annovar'
+		var inputFile = 'test.vcf';
+		var tempOutputFile = inputFile + '.hg19_multianno.txt';
+		var outputFile = 'test.json';
+		var tableName = 'test2';
+		var databaseString = 'refgene';
+		var dbusageString = 'g';
+
+
+	
+		fs.statAsync(inputFile)
+		.then(function(){
+			return fs.statAsync(annovarPath);
+		})
+		.then(function(){
+			return db.connect('mongodb://localhost:27017/patientDB').then(function(db){
+				console.log("Connected to Database")
+			});
+		})
+		.then(function(){
+			return db.createTable(tableName).then(function(db){
+				console.log("Table Created");
+			});
+		})
+		.then(function(){
+			var annovarCmd = 'perl '  + annovarPath + '/table_annovar.pl ' +  
 				inputFile + ' ' + annovarPath + '/humandb/ -buildver hg19 -operation ' + dbusageString + '  -nastring . -vcfinput ' + 
 				'-protocol  ' + databaseString
-
-
-
 			console.log("Annotation Command::: " + annovarCmd);
 			console.log("Annotating File...");
-
 			return child_process.execAsync(annovarCmd)
-		}).then(function(err,stdout,stderr){
-			if (stderr != null){
-				throw new AnnotationError(err);	
-			} 
-		}).then(fs.statAsync(tempOutputFile))
+			})
+		.then(function(){
+			return fs.statAsync(tempOutputFile)
+		})
 		.then(function(){
 			console.log("Annotation complete");
-			var parserCmd = PYTHONPATH + ' ./parser.py ' + tempOutputFile + " " + outputFile;
+			var parserCmd = 'python ./parser.py ' + tempOutputFile + " " + outputFile;
 			console.log("Parsing annotated output...");
 			return child_process.execAsync(parserCmd);
-		}).then(function(err,stdout,stderr){
+			})
+		.then(function(err,stdout,stderr){
 			if (stderr != null){
 				throw new ParseError(err);
 			} 
-			console.log("Loading Parsed entries...");
-		}).then(db.createTable(tableName).catch(function(err){throw new Error(err)}))
-		.then(fs.statAsync(outputFile))	
-		.then(fs.readfileAsync(tempOutputFile))
-		.then(JSON.parse)
-		.each(function(entry){
-			db.insert({'tableName':tableName,documents:entry})
+			})
+		.then(function(){
+			return fs.statAsync(outputFile);
+		})	
+		.then(function(){
+			console.log("Reading json data")
+		})
+		.then(function(){
+			return fs.readFileAsync(outputFile).then(JSON.parse)
+		})
+		.then(function(data){
+			var tempList = []
+			var maxInput = data.length
+			//bulk write operations are currently limited to 1000 entries,
+			//therefore you need to split the entries int smaller chuncks
+			for (var i = 0; i < maxInput; i+=1000){
+				var max;
+				if (i + 999 > maxInput){
+					max = maxInput;
+				} else {
+					max = i + 1000;
+				}
+				tempList.push(data.slice(i,max))
+			} 
+			return tempList
+		})
+		.each(function(docs){
+			return db.insertMany({tableName:tableName,documents:docs});	
+		})
+		.then(function(docs){
+			var totalLength= 0;
+			for (var i = 0; i < docs.length; i++)
+				totalLength += docs[i].length;
+			console.log('Total Items Inserted into ' + tableName + ': ' + totalLength);
+		})
+		.then(function(){
+			return glob.globAsync(inputFile + ".*");
+		})
+		.each(function(file){
+			return fs.unlinkAsync(file);
+		})
+		.then(function(){
+			return fs.unlinkAsync(outputFile);
+		})
+		.then(function(){
+			console.log("Cleanup Successfull");
 		}).catch(function(err){
 			console.log(err)
-		}).done(resolve("completed data insert"))
+		})	
+		.done(function(){
+			db.db.close();
+			resolve("completed data insert");
+		})
+			
 	});
 	
 	return promise;
 }
 
-main().catch(function(err){console.log(err)})
-
+exports.annotateAndAddVariants = annotateAndAddVariants
 
 	
 		
