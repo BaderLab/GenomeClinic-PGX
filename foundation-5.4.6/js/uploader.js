8d1ae33fa19e167d4360e3bbbbb69410de4e15e6
Patrick Magee <patrickmageee@gmail.com>
2015-01-15 16:04:33 -0500

    Continued work on the Jquery uploader html and the javascript

--- foundation-5.4.6/js/uploader.js
+++ foundation-5.4.6/js/uploader.js
@@ -11,6 +11,17 @@ var buttonHandlers = function(){
     }
   });
 
+  $('#sex-switch').prop('checked', false);
+  $('#sex-switch-value').text("Male");
+
+  $(document).on('change','#sex-switch',function(){
+    if ($(this).prop("checked")){
+      $("#sex-switch-value").text("Female");
+    } else {
+      $("#sex-switch-value").text("Male");
+    }
+  })
+
   
   $("#select-all").on("mouseup",function(){
      $('.gene').addClass("alert")
@@ -26,9 +37,55 @@ var buttonHandlers = function(){
 
 
 var uploader = function(){
+  var jqXHR;
+
   $("#upload-button").on("click",function(){
     $("#fileselect").trigger('click');
   });
+
+
+  $("#cancel-button").on("click",function(){
+    if($("#file-to-upload").hasClass("working"))
+      jqXHR.abort()
+
+    $(this).closest('.button-group').toggle().parents().find('#upload-button').toggle()
+    .parents().find("#upload-box").empty();
+
+   });
+
+  $("#fileselect").fileupload({
+    url:'upload/vcf',
+    add: function(e,data){
+      global = data  
+      
+      var fileInfo = $("<p id='file-to-upload'></p>");
+
+      fileInfo.text(data.files[0].name)
+      .append('&nbsp<i>' + data.files[0].size/1000 + 'kb</i>');
+      data.context = $("#upload-box").html(fileInfo);
+
+      $('#upload-box').show();
+      $('#upload-button').toggle().parents().find(".button-group").toggle();
+
+      $("#submit-button").on('click',function(){
+        $('#file-to-upload').addClass("working")
+        $("#  submit-button").off('click')
+        jqXHR = data.submit()
+
+      });
+    },
+    progress: function(e,data){
+      var progress = parseInt(data.loaded/data.total * 100,10);
+
+        console.log(progress);
+
+    },
+
+    fail: function(e,data){
+      console.log(e);
+      data.context.addClass('error')
+    }
+  })
 }
 
 
