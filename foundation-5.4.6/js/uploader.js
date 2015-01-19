c4cf64ed1200cc32cbe7ba0aaed57ad1c4d074ac
patmagee <patrick.magee@mail.utoronto.ca>
2015-01-19 16:30:03 -0500

    Completed uploaded Jquery module. This script contains all the handlers required for navigating to and uploading data to the remote server

--- foundation-5.4.6/js/uploader.js
+++ foundation-5.4.6/js/uploader.js
@@ -1,16 +1,35 @@
+/* Jquery uploader moduke and event handlers for uploading a new
+ * Patient to the server, annotating then inputting it into
+ * the current database
 
 
+ * requires: 
+
+ * jquery.ui.widget.js
+ * jquery.iframe-transport.js
+ * jquery.fileupload.js
+
+ * from the jquery-file-upload.
+ * https://github.com/blueimp/jQuery-File-Upload
+
+
+ *written by Patrick Magee
+*/
+
+/* adding the endsWith functionallity to strings for cross platform compatibility
+*/
 String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
 
 
-
+//general form handlers
 var formHandlers = function(){
   $('#sex-switch').prop('checked', false);
   $('#sex-switch-value').text("Male");
 
+  //Sex switch hanlder convers between male and femal
   $(document).on('change','#sex-switch',function(){
     if ($(this).prop("checked")){
       $("#sex-switch-value").text("Female");
@@ -19,6 +38,9 @@ var formHandlers = function(){
     }
   });
 
+  //Check to see if the user input is currently already in
+  //the patient databse. If it is, add the rror class
+  //to the input container.
   $('#patient_id').on("keyup",function(){
     var promise;
     var keyValue = $(this).val().toString();
@@ -45,49 +67,56 @@ var formHandlers = function(){
 
 
 
+//Validate the input on the form and parse the form data
+//into and output object to give to the upload file handler
+//returns a promise.
+var validateForm = function(uploadData){
+  var returnData = {};
+  var data = $('#jquery-new-patient-form').serializeArray();
 
-var validateAndSendForm = function(){
-  var data = {};
-  var dataTmp = $('#jquery-new-patient-form').serializeArray();
-  for (var i = 0; i< dataTmp.length;i++){
-    if (dataTmp[i]['value'] != ""){
-      if (dataTmp[i]['name'] == 'age')
-        data[dataTmp[i]['name']] = parseInt(dataTmp[i]['value'],10);
-      else
-        data[dataTmp[i]['name']] = dataTmp[i]['value'];
-    }
-  }
-  data['sex'] = $('#sex-switch-value').text().toLowerCase();
 
-  if (!data['patient_id']){
+  if (data[0]['value']===""){
     $('#patient_id').addClass('error').parents().find("small").text("Required").show()
     return Promise.reject().then(function(err){
       throw new Error('Patient Id required')
     });
+
+  } else if ($('#patient_id').hasClass('error')){
+
+
+    return Promise.reject().then(function(err){
+      throw new Error("Patient Id Already used")
+    });
+
+  } else {
+
+    for (var i=0;i<data.length;i++){
+      returnData[data[i]['name']] = data[i]['value'];
+    }
+
+    returnData['sex'] = $('#sex-switch-value').text();
+    returnData['age'] = $('#sliderOutput').text();
+
+    return Promise.resolve(returnData);
   }
-
-  var promise= Promise.resolve($.ajax({
-    url: 'database/addPatient',
-    type: "POST",
-    contentType: "application/json",
-    dataType: "text",
-    data: JSON.stringify(data)
-  }));
-
-  return promise
 };
 
 
 
+
+//Main uploader function. Handles the ajax request to upload a file
+//In an async manner.
 var uploader = function(){
   var jqXHR;
-  var patientTableName;
 
+
+  //Uploadt button event handler for triggering the hidden file selection
   $("#upload-button").on("click",function(){
     $("#fileselect").trigger('click');
   });
 
 
+  //Cancel this data transfer AND take back to add file button
   $("#cancel-button").on("click",function(){
     if($("#file-to-upload").hasClass("working"))
       jqXHR.abort()
@@ -97,6 +126,8 @@ var uploader = function(){
 
   });
 
+
+  //Upload file with handlers for addition, failure,progress and completeiong of the upload
   $("#fileselect").fileupload({
     url:'/upload/vcf',
     add: function(e,data){
@@ -107,40 +138,49 @@ var uploader = function(){
         alert("Invalid File, please choose a file that ends in .vcf extension");
       } else {
         if (name.length > 20){
-            name = "..." + name.substr(-20)
+            name = "..." + name.substr(-20);
          } 
 
         $('#file-to-upload').text(name)
         .append('&nbsp<i>' + data.files[0].size/1000 + 'kb</i>');
-        data.context = $("#upload-box")
+        data.context = $("#upload-box");
 
-        $('#upload-box').show();
+
+        $('.progress').removeClass('success').children("#upload-progress").css({'width':'0%'});
+        $('#upload-box').show().children('.progress').find('#upload');
         $('#upload-button').toggle().parents().find(".button-group").toggle();
 
+        //Submit the file for uploading
         $("#submit-button").on('click',function(){
-          var status = validateAndSendForm();
-          status.then(function(result){
-            $('#file-to-upload').addClass("working")
-            $("#submit-button").off('click')
-            jqXHR = data.submit()
+          var status = validateForm(data);
+          status.then(function(uploadData){
+            data.formData = uploadData;
+            $('#file-to-upload').addClass("working");
+            $("#submit-button").off('click');
+            jqXHR = data.submit();
+
+            return jqXHR;
           }).catch(function(err){
-            console.log(err)
+            console.log(err);
           });
         });
       }
     }, 
 
+    //update a progress bar to track the upload process
     progress: function(e,data){
       var progress = parseInt(data.loaded,10)/parseInt(data.total,10)*100
       $('#upload-progress').animate({width:progress + '%'},0);
     },
 
+    //when the upload fails add an error class to the upload bar
     fail: function(e,data){
       $('.progress').hide().children("#upload-progress").css({'width':'0%'});
       $('#file-to-upload').removeClass('working').append("&nbsp&nbsp<span class='error'><i>File failed to upload properly</i></span>");
 
     },
 
+    //upon completion add checkmark and a 
     done: function(e,data){
       $('.progress').addClass('success');
       $('#file-to-upload').removeClass('working').append("&nbsp&nbsp<i class='fi-check size-16'><i>");
@@ -149,19 +189,21 @@ var uploader = function(){
 }
 
 
+//Render the page htmnl and display it adding handlers at the same time
+var addPatientHtml = function(){
+  var promise = new Promise(function(resolve,reject){
+    var html= renderHbs('frangipani-add-new-patient.hbs');
+    settings.applicationMain.html(html);
+    $(document).foundation(); //have to call this to enable the slider
+    formHandlers();
+    uploader();
+    resolve();
+  });
+  return promise;
+}
 
+var addAllEventListeners = function(){
+  clickAction($('#frangipani-add-new-patient'), addPatientHtml); 
+}
 
-var main = function(){
-  formHandlers();
-  uploader()
-};
-
-
-
-
-
-
-
-
-
-$(document).ready(main)
+$(document).ready(addAllEventListeners);
