38c20fcb6e0606566912eaa707ccb365bde549ee
patmagee <patrick.magee@mail.utoronto.ca>
2015-02-24 15:12:57 -0500

    Main projects javascript handlers. This module facilitates all interactions with the projects on the client side, including rendering, hanlders etc.

--- public/js/frangipani-projects.js
+++ public/js/frangipani-projects.js
@@ -1,5 +1,5 @@
 (function(){
-	var patientInformation, projectInfo;
+	var patientInformation, projectInfo, projectName;
 	var selected = [];
 
 
@@ -26,11 +26,49 @@
 		else if (input.match(re) != null)
 			return true
 		return false
-	}		
+	}
 
-	var refreshNum = function(){
-		var rows = $('.patient-row:visible');
-		$('#number-of-patients').text(rows.length);
+	var getPatientInformation = function(project,excluded){
+		var options = {
+					'project':project,
+					'exclude':excluded
+		};
+
+		return Promise.resolve($.ajax({
+			url: "/patients",
+			type: "POST",
+			contentType: "application/json",
+			dataType: 'json',
+			data:JSON.stringify(options)
+		}))
+
+	};
+	
+	var renderPatientInformation = function(project,excluded,ele,handlers,callback){
+		return getPatientInformation(project,excluded)
+		.then(function(result){
+			patientInformation = result.map(function(item){
+				return item['patient_id'];
+			})
+			var options = {
+				patients:result,
+				project:true
+			}
+			return asyncRenderHbs('frangipani-patients.hbs',options)
+		}).then(function(renderedHtml){
+			$(ele).html(renderedHtml).find(document).foundation();
+		}).then(function(){
+			if (handlers)
+				return handlers(ele);
+		}).then(function(){
+			if (callback)
+				callback(ele)
+		});
+	};		
+
+	var refreshNum = function(ele){
+		var rows = $(ele).find('.patient-row:visible');
+		$(ele).find('#number-of-patients').text(rows.length);
 	}		
 
 	var patientLinkHandler = function(id){
@@ -48,8 +86,8 @@
 		})
 	}
 
-	var patientRowHandler = function(){
-		$('.patient-row').on('click',function(){
+	var patientRowHandler = function(ele){
+		$(ele).find('.patient-row').on('click',function(){
 			$(this).hide();
 			var val = parseInt($(this).attr('id').replace('row-',''));
 			selected.push(patientInformation[val]);
@@ -58,35 +96,60 @@
 			$('#patient_id_links').append(html)
 			patientLinkHandler('#link-' + val);
 
-			refreshNum()
-		})
-	}
+			refreshNum(ele);
+		});
+	};
 
-	var searchBoxHandler = function(){
+	var searchBoxHandler = function(ele){
 		$('#searchBox').on('keyup',function(){
 			for (var i=0; i < patientInformation.length; i++){
 				if (matchSearch(patientInformation[i]) && selected.indexOf(patientInformation[i]) == -1 )
-					$('#row-' + i).show();
+					$(ele).find('#row-' + i).show();
 				else 
-					$('#row-' + i).hide();
+					$(ele).find('#row-' + i).hide();
 			}
-
-			refreshNum()
-		})
-	}
+			refreshNum(ele);
+		});
+	};
 
 	var closeBox = function(){
 		$('.close-box').on('click',function(e){
 			e.preventDefault();
-			$(this).closest('.alert-box').hide();
+			$(this).closest('.alert-box').hide().removeClass('alert');
+		});
+	}
+
+	var confirm = function(ele, modal, clickFunction, callback){
+		$(ele).on('click',function(e){
+			e.preventDefault();
+			var result = true;
+			if (clickFunction)
+				result = clickFunction(modal);
+
+			console.log(result);
+			if(result){
+				console.log($(modal));
+				$(modal).find('.confirm').on('click',function(e){
+					e.preventDefault();
+					$(this).off('click');
+					$(modal).foundation('reveal','close');
+					callback(true,result)					
+				});
+				$(modal).find('.cancel').on('click',function(e){
+					e.preventDefault();
+					$(this).off('click');
+					$(modal).foundation('reveal','close');
+					callback(undefined,result);
+					
+				});
+				$(modal).foundation('reveal','open');
+			}
 		});
 	}
 
 	var addNewProjectHandlers = function(){
-
-		patientRowHandler();
-		searchBoxHandler();
-	
+		patientRowHandler('#patient-information');
+		searchBoxHandler('#patient-information');
 		$('#add-auth-user').on('click',function(e){
 			e.preventDefault();
 			var val = $('#new-user').val();
@@ -192,126 +255,109 @@
 
 	};
 
-	var projectPageHandlers = function(projectName){
+	var projectPageHandlers = function(){
 		closeBox();
-
-		$('#remove-patients').on('click',function(e){
-			e.preventDefault();
-			var checked  = $('input:checked').map(function(){
+		confirm('#remove-patients','#confirm-removal',function(modal){
+			var checked = $('input:checked').map(function(){
 				return $(this).data('id').toString();
 			}).toArray();
 			if (checked.length > 0){
-				$("#confirm-removal").find("h3").text("Are you sure you want to remove " + checked.length + " patients from " + projectName)
-				
-				$("#confirm-removal").find(".confirm").on('click',function(e){
-					e.preventDefault();
-					$(this).off('click');
-					var data = {
+				$(modal).find('h3').text("Are you sure you want to remove " + checked.length + " patients from " + projectName)
+				return checked;
+			}
+			return undefined;
+		}, function(confirm,checked){
+			if (confirm){
+				var data = {
 						'patients':checked,
 						'project':projectName
-
-					}
-
-					var promise = Promise.resolve($.ajax({
-						url:'/database/projects/removepatients',
-						type:'POST',
-						contentType:'application/json',
-						dataType:'json',
-						data:JSON.stringify(data)
-					}))
-
-					promise.then(function(result){
-						$('#confirm-removal').foundation('reveal','close');
-						$('input:checked').closest('tr').remove()
-						$('#error-display-message').text("Successfully removed " + checked.length + " patients from " + projectName)
-						.parents().find("#error-display-box").show();
-						refreshNum();
-					});
-					
+				};
+				Promise.resolve($.ajax({
+					url:'/database/projects/removepatients',
+					type:'POST',
+					contentType:'application/json',
+					dataType:'json',
+					data:JSON.stringify(data)
+				})).then(function(result){
+					$('input:checked').closest('tr').remove()
+					$('#error-display-message').text("Successfully removed " + checked.length + " patients from " + projectName)
+					.parents().find("#error-display-box").show();
+					refreshNum('#patients-table');
 				})
-
-				$("#confirm-removal").find('.cancel').on('click',function(e){
-					e.preventDefault();
-					$(this).off('click');
-					$('#confirm-removal').foundation('reveal','close');
-				})
-
-				$('#confirm-removal').foundation('reveal','open');
 			}
-		});
-
-		$('#add-patients').on('click.modal',function(e){
-			selected = [];
-			e.preventDefault();
+		})
 		
-			getPatientInformation(projectName,true).then(function(result){
-				patientInformation = result.map(function(item){
-					return item['patient_id'];
-				})
-				var options = {patients:result};
-				return asyncRenderHbs('frangipani-patients.hbs',options)
-			}).then(function(renderedHtml){
-				$('#add-patients-modal').find("#patient-information").html(renderedHtml);
-				patientRowHandler();
-				searchBoxHandler();
+		confirm('#add-patients','#add-patients-modal',function(modal){
+			selected = [];
+			return renderPatientInformation(projectName,true,'#patient-information',function(){
+				patientRowHandler('#patient-information');
+				searchBoxHandler('#patient-information');
 				$('.patient-row').on('click.resize', function(){
 					if ($('#patient_id_links').height() > 150) {
 						$('#patient_id_links').addClass('scrollit');
 					} else {
 						$('#patient_id_links').removeClass('scrollit');
 					}
-				})
-			});
-			$('#add-patients-modal').find('.cancel').on('click.modal',function(e){
-				e.preventDefault();
-				selected = [];
-				$(this).off('click.modal');
-				$('#add-patients-modal').find('#patient_id_links').empty();
-				$('#add-patients-modal').find('#patient-information').empty();
-				$('#add-patients-modal').foundation('reveal','close');
-				$('.patient-row').off('click.modal');
-			});
-
-			$('#add-patients-modal').find('.confirm').on('click.modal',function(e){
-				e.preventDefault();
-				$(this).off('click.modal');
+				});
+				return true;
+			})
+		},function(confirm,result,modal){
+			if (confirm){
 				var options = {
 					patients:selected,
 					project:projectName
 				}
-				var promise = Promise.resolve($.ajax({
+
+				Promise.resolve($.ajax({
 					url:'/database/projects/addpatients',
 					type:'POST',
 					contentType:"application/json",
 					dataType:'json',
 					data:JSON.stringify(options)
-				}))
-				promise.then(function(result){
-					$('#add-patients-modal').find('#patient_id_links').empty();
-					$('#add-patients-modal').find('#patient-information').empty();
-					$('#add-patients-modal').foundation('reveal','close')
+				})).then(function(result){
+					$(modal).find('#patient_id_links').empty();
+					$(modal).find('#patient-information').empty();
 					$('#error-display-message').text("Successfully added " + selected.length + " patients to " + projectName)
 						.parents().find("#error-display-box").show();
-					refreshNum();
 					selected = [];
+					$(modal).find('#patient_id_links').empty().removeClass('scrollit');
+					$(modal).find('#patient-information').empty();
 				}).then(function(){
-					return getPatientInformation(projectName);
-				}).then(function(result){
-					var options = {
-						patients:result,
-						project:true
-					};
-					return asyncRenderHbs('frangipani-patients.hbs',options)
-				}).then(function(renderedHtml){
-					$('#patients-table').html(renderedHtml);
-					$('.patient-row').off('click.modal');
+					return renderPatientInformation(projectName,undefined,'#patients-table',function(){
+						$('.patient-row').off('click')
+					})
 				});
-			});
-			$('#add-patients-modal').foundation('reveal','open');
-		});
+			} else {
+				selected = [];
+				$(modal).find('#patient_id_links').empty().removeClass('scrollit');
+				$(modal).find('#patient-information').empty();
+				$('.patient-row').off('click')
+			}
+		})
 
+		confirm('#delete-project','#confirm-removal',function(modal){
+			$(modal).find('h3').text("Are you sure you want to delete this project? the operation is permanent and you will not be able to recover it afterwards. You will be redirected if the request is successful")
+			return true;
+		}, function(confirm,result,modal){
+			if (confirm){
+				Promise.resolve($.ajax({
+					url:'/database/projects/delete',
+					type:'POST',
+					contentType:'application/json',
+					dataType:'json',
+					data:JSON.stringify({project:projectName})
+
+				})).then(function(result){
+					if (result.statusCode == '200'){
+						window.location.replace((result.redirectURL || '/projects'))
+					} else {
+						$('#error-display-message').text(result.error).addClass('alert')
+						.parents().find("#error-display-box").show();
+					}
+				});
+			}
+		})	
 	};
-
 	var allProjectPageHandlers = function(){
 		$('#add-new-project').on('click',function(e){
 			e.preventDefault();
@@ -340,14 +386,14 @@
 		})
 
 		$('.project-row').on('click',function(){
-			var project = $(this).find('.frangipani-project-name').text();
+			projectName = $(this).find('.frangipani-project-name').text();
 			var promise = Promise.resolve($.ajax({
 				url:'/database/projects',
 				type:'POST',
 				contentType:'application/json',
 				dataType:'json',
 				data:JSON.stringify({
-					'project_id':project
+					'project_id':projectName
 					})
 			}))
 
@@ -358,7 +404,7 @@
 			}).then(function(renderedHtml){
 				$("#page-content").html(renderedHtml);
 			}).then(function(){
-				return getPatientInformation(project);
+				return getPatientInformation(projectName);
 			}).then(function(result){
 				var options = {
 					patients:result,
@@ -369,7 +415,7 @@
 			}).then(function(renderedHtml){
 				$('#patients-table').html(renderedHtml);
 				$(document).foundation();
-				projectPageHandlers(project);
+				projectPageHandlers();
 			}).catch(function(err){
 				console.log(err);
 			});
@@ -377,22 +423,8 @@
 
 	};
 
-	var getPatientInformation = function(project,excluded){
-		var options = {
-					'project':project,
-					'exclude':excluded
-		};
-
-		return Promise.resolve($.ajax({
-			url: "/patients",
-			type: "POST",
-			contentType: "application/json",
-			dataType: 'json',
-			data:JSON.stringify(options)
-		}))
-
-	};
 	
+
 	var loadhtml = function(){
 		var promise = Promise.resolve($.ajax({
 			url:'/database/projects',
