7431cf5932aa6a166bd4b942d88b6331f1f4252d
Patrick Magee <patrickmageee@gmail.com>
2015-03-26 17:56:11 -0400

    removed unnecessary testing files

--- src/client/templates/test.js
+++
@@ -1,8 +0,0 @@
-var Promise = require('bluebird');
-var cons = Promise.promisifyAll(require('consolidate'));
-
-cons.handlebarsAsync('./layout.hbs',{}).then(function(html){
-	console.log(html);
-}).catch(function(err){
-	console.log(err)
-});
