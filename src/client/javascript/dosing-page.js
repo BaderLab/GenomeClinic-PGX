1b7eebd0fdda6c7ac012b86bfc318a6f6b30466a
Patrick Magee <patrickmageee@gmail.com>
2015-04-21 12:27:20 -0400

    Added template and js to current dosing

--- src/client/javascript/dosing-page.js
+++ src/client/javascript/dosing-page.js
@@ -14,8 +14,39 @@ module.exports = function(){
 						$(currentRows[i]).show();
 				}
 			});
+
+			$('.dose-row').on('click',function(e){
+				e.preventDefault();
+				var location = '/dosing/current/' + $(this).data('name');
+				window.location.replace(location);
+			});
+		},
+		current:function(){
+			var originalCollapseButtonColor = "rgb(0, 123, 164)";
+			var expand= function(element) {
+				$(element).data("expanded", true);
+				$(element).css("color", "#FFAD99");
+				$(element).css("transform", "rotate(45deg)");
+				$(element).closest('.drug-cont').find('.interactions').slideToggle();
+			};
+
+			var collapse= function(element) {
+				$(element).data("expanded", false);
+				$(element).css("color", originalCollapseButtonColor);
+				$(element).css("transform", "rotate(0deg)");
+				$(element).closest('.drug-cont').find('.interactions').slideToggle();
+			};
+
+			$('.drug-expand').on('click',function(e){
+				e.preventDefault();
+				if ($(this).data('expanded')){
+					collapse(this);
+				} else {
+					expand(this);
+				}
+			});
 		}
-	}
+	};
 
 
 	var main = function(){
@@ -34,6 +65,36 @@ module.exports = function(){
 			}).then(function(){
 				staticHanlders.index();
 			});
+		} else if (location.search(/^\/dosing\/current\/.*/) !== -1 ){
+			var gene = location.split('/').splice(-1)[0]
+			promise = Promise.resolve($.ajax({
+				url:"/database/dosing/genes/" + gene,
+				type:'GET',
+				contentyType:'application/json',
+				dataType:'json'
+			})).then(function(result){
+				//Arrange the output by drug.
+				var drugOutput = {};
+				var drug;
+				for ( var i=0; i<result.length; i++ ){
+					drug = result[i].drug
+					if (!drugOutput.hasOwnProperty(drug)){
+						drugOutput[drug] = [];
+					}
+					drugOutput[drug].push(result[i]);
+				}
+				return drugOutput;
+			}).then(function(result){
+				console.log(result);
+				var o = {};
+				o.drugs = result;
+				o.gene = gene.toUpperCase()	;
+				return templates.drugs.current(o);
+			}).then(function(renderedHtml){
+				return $('#main').html(renderedHtml);
+			}).then(function(){
+				staticHanlders.current();
+			});	
 		}
 
 		return promise;
