f84f0f66010b46f28554e8d92c1316ca4dc844bd
Patrick Magee <patrickmageee@gmail.com>
2015-06-08 18:56:39 -0400

    added sort function

--- src/server/src/utils.js
+++ src/server/src/utils.js
@@ -143,4 +143,18 @@ module.exports = {
 	        }
 	    })
 	},
+	sortWithIndeces: function(toSort) {
+	  for (var i = 0; i < toSort.length; i++) {
+	    toSort[i] = [toSort[i], i];
+	  }
+	  toSort.sort(function(left, right) {
+	    return left[0] < right[0] ? -1 : 1;
+	  });
+	  toSort.sortIndices = [];
+	  for (var j = 0; j < toSort.length; j++) {
+	    toSort.sortIndices.push(toSort[j][1]);
+	    toSort[j] = toSort[j][0];
+	  }
+	  return toSort;
+	}
 };
