4522c7d0c5ac2dec730bd8fff42eb769c9746413
Patrick Magee <patrickmageee@gmail.com>
2015-06-18 17:56:21 -0400

    removed old logger file

--- src/server/src/anno_logger.js
+++
@@ -1,4 +0,0 @@
-/* wrapper to load an annotation specific logger since requiring this logger
- * loads a cached version if not done in this manner */
-var logger = require('./logger')('annotation');
-module.exports = logger;
