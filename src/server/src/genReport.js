af49ddb35ee86775d9a805ba3cbc49180641cf02
patmagee <patrick.magee@mail.utoronto.ca>
2015-07-16 16:01:08 -0400

    fixed minor bug on pdf report generation

--- src/server/src/genReport.js
+++ src/server/src/genReport.js
@@ -17,7 +17,7 @@ var fs = Promise.promisifyAll(require('fs'));
  * can sometimes take a few seconds leading to noticable lag on the client side. Once the file has been rendered and 
  * saved in the temp folder, a response is sent to the client, with the reportName to retrieve from the server.
  */
-module.exports = function(req,res,reportName,template,options){
+module.exports = function(req,res,reportName,template,options,logger){
 	var name,path,top,bottom,left,right,format,orientation;
 
 	//Page size and margin parameter defaults
